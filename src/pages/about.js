75b43f2cbd1ac08ec194becd745886ab53988ea4
doo9392 <hackish@hanmail.net>
2020-12-24 15:36:29 +0900

    [improved]: about 페이지 정리

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,29 +8,7 @@ const AboutPage = () => {
     <Layout>
       <SEO title="About Blog Boost Starter" />
       <div id="about">
-        <h2>About Blog Boost Starter</h2>
-        <p>
-          <strong>Gatsby Starter Blog Boost</strong> is a Netlify CMS powered
-          Gatsby Blog with a wide range of features already configured.
-        </p>
 
-        <p>
-          This starter is a great way to get blogging using a content manager
-          while learning and practicing <strong>JavaScript</strong>,{" "}
-          <strong>React</strong>, <strong>Gatsby</strong>, and{" "}
-          <strong>GraphQL</strong>,
-        </p>
-
-        <div className="about-icons">
-          <span role="img" aria-label="Laptop.">
-            💻
-          </span>
-          <span role="img" aria-label="Rocket.">
-            🚀
-          </span>
-        </div>
-
-        <h3>Thanks for visiting!!</h3>
       </div>
     </Layout>
   )
